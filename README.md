6f35acb5054b3a3c228e9e8f7eb866cfec1df178
Kevin Huang <kevihuang@microsoft.com>
2023-09-11 16:59:17 -0700

    Added code to extract problems from excerpts of a paper

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Microsoft Hackathon 2023
 1. Ensure that you have Python installed
 2. Run 'pip install -r requirements.txt'
 3. Go to search_msft_hackathon.ipynb and start running the code
+
+# To make your own branch
+1. Run 'git checkout -b <branch-name>'
--- llm.py
+++ llm.py
@@ -5,15 +5,18 @@
 import dotenv
 import os
 import openai
+import time
 
 dotenv.load_dotenv()
 # openai.api_key = os.getenv("OPENAI_GPT4_API_KEY")
 openai.api_type = "azure"
-openai.api_key = "74d96cceffae45ddbeb8934844f6a371" # GPT3.5-16k key
+openai.api_version = "2023-05-15" 
+openai.api_base = "https://americasopenai.azure-api.net/"  # Your Azure OpenAI resource's endpoint value.
+openai.api_key = "74d96cceffae45ddbeb8934844f6a371"
 
 # models
 EMBEDDING_MODEL = "text-embedding-ada-003"
-GPT_MODEL = "gpt-3.5-turbo"
+GPT_MODEL = "gpt-35-turbo-16k"
 # GPT_MODEL = "gpt-4"
 
 # for bulk openai message, no stream
@@ -43,27 +46,61 @@ def chat_openai_high_temp(prompt="Tell me to ask you a prompt", model=GPT_MODEL,
     return text_answer, messages
 
 def chat_openai(prompt="Tell me to ask you a prompt", model=GPT_MODEL, chat_history=[], temperature=0, verbose=False):
-    # define message conversation for model
-    messages = [
-        {"role": "system", "content": "You are a helpful and educated carbon capture research consultant and an educated and helpful researcher and programmer. Answer as correctly, clearly, and concisely as possible. You give first-rate answers."},
-    ]
-    if chat_history:
-        messages += chat_history
-    messages.append({"role": "user", "content": prompt})
+    # To deal with rate limits, just wait
+    while True:
+        try:
+            # define message conversation for model
+            messages = [
+                {"role": "system", "content": "You are a helpful and educated carbon capture research consultant and an educated and helpful researcher and programmer. Answer as correctly, clearly, and concisely as possible. You give first-rate answers."},
+            ]
+            if chat_history:
+                messages += chat_history
+            messages.append({"role": "user", "content": prompt})
 
-    # create the chat completion
-    if verbose:
-        print("Prompt messages: ", messages)
-    completion = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        temperature=temperature,
-    )
-    if verbose:
-        print("Completion info: ", completion)
-    text_answer = completion["choices"][0]["message"]["content"]
+            # create the chat completion
+            if verbose:
+                print("Prompt messages: ", messages)
+            completion = openai.ChatCompletion.create(
+                engine=GPT_MODEL,
+                messages=messages,
+                temperature=temperature,
+            )
+            if verbose:
+                print("Completion info: ", completion)
+            text_answer = completion["choices"][0]["message"]["content"]
 
-    # updated conversation history
-    messages.append({"role": "assistant", "content": text_answer})
-
-    return text_answer, messages
+            # updated conversation history
+            messages.append({"role": "assistant", "content": text_answer})
+            return text_answer, messages
+        except openai.error.RateLimitError:
+            print(
+                "   *** The OpenAI API rate limit has been exceeded. Waiting 10 seconds and trying again. ***"
+            )
+            time.sleep(10)  # Wait 10 seconds and try again
+        except openai.error.Timeout:
+            print(
+                "   *** OpenAI API timeout occurred. Waiting 10 seconds and trying again. ***"
+            )
+            time.sleep(10)  # Wait 10 seconds and try again
+        except openai.error.APIError:
+            print(
+                "   *** OpenAI API error occurred. Waiting 10 seconds and trying again. ***"
+            )
+            time.sleep(10)  # Wait 10 seconds and try again
+        except openai.error.APIConnectionError:
+            print(
+                "   *** OpenAI API connection error occurred. Check your network settings, proxy configuration, SSL certificates, or firewall rules. Waiting 10 seconds and trying again. ***"
+            )
+            time.sleep(10)  # Wait 10 seconds and try again
+        except openai.error.InvalidRequestError:
+            print(
+                "   *** OpenAI API invalid request. Check the documentation for the specific API method you are calling and make sure you are sending valid and complete parameters. Waiting 10 seconds and trying again. ***"
+            )
+            time.sleep(10)  # Wait 10 seconds and try again
+        except openai.error.ServiceUnavailableError:
+            print(
+                "   *** OpenAI API service unavailable. Waiting 10 seconds and trying again. ***"
+            )
+            time.sleep(10)  # Wait 10 seconds and try again
+        else:
+            break
